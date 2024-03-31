8236374f64bc079fe0685b2fce8f18effc3d9774
Hassam Ullah <malikhassamullah1@yahoo.com>
2024-03-31 18:25:18 +0500

    add ssl certificate

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import https from "https"
+import fs from "fs"
 import bodyParser from "body-parser";
 import { Config } from "./services/config.service";
 import {
@@ -1492,15 +1494,23 @@ async function run_server() {
       });
   });
 
-  const server = app.listen(
-    Number(serverConfig.port),
-    serverConfig.host,
-    () => {
-      console.log(
-        `[server]: Server is running at http://${serverConfig.host}:${serverConfig.port}`,
-      );
-    },
-  );
+  const server = https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+  }, app).listen(Number(serverConfig.port), serverConfig.host, () => {
+    console.log(
+      `[server]: Server is running at https://${serverConfig.host}:${serverConfig.port}`,
+    );
+  });
+  // const server = app.listen(
+  //   Number(serverConfig.port),
+  //   serverConfig.host,
+  //   () => {
+  //     console.log(
+  //       `[server]: Server is running at http://${serverConfig.host}:${serverConfig.port}`,
+  //     );
+  //   },
+  // );
 
   const io = new Server(server, {
     // pingTimeout: 360000,
