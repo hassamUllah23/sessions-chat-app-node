ec6ed89ebdd7bdf183456fc930ddbbef8d60ffbf
Hassam Ullah <malikhassamullah1@yahoo.com>
2024-04-04 13:42:27 +0500

    remove console logs

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import { close_session } from "./modules";
 
 (async () => {
   const args = process.argv[2];
-  console.log({ args: process.argv });
   if (args === "-cs" || args === "--close-session") {
-    console.log("we inside cs");
     try {
       const sessionIdIndex = process.argv.indexOf("--sessionId");
       const sessionId = process.argv[sessionIdIndex + 1];
-      console.log({ sessionIdIndex, sessionId });
+      console.log({ sessionId });
 
       await close_session({ sessionId: sessionId });
     } catch (error) {
@@ -18,7 +16,6 @@ import { close_session } from "./modules";
       process.exit(0);
     }
   } else if (args === "-s" || args === "--server") {
-    console.log("starting server from new location");
     await run_server();
   }
 })();
